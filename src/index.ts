690e48e9fc980a38889b62f495f118bb5a55add1
dosite-cyiza <dositecyiza@gmail.com>
2025-11-27 18:57:04 +0200

    multiple of 3 or 5

--- src/index.ts
+++ src/index.ts
@@ -494,3 +494,19 @@ for even numbers or "Odd" for odd numbers.
  return (n % 2 === 0)? "Even":"Odd"
 }
 console.log(evenOrOdd(8))
+
+/*
+Multiple of 3 or 5
+*/
+function solution(number: number) {
+    let sum:number = 0
+   for (let i = 0; i < number; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+
+    return sum;
+}
+console.log(solution(10))
+    
